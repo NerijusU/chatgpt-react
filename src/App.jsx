085fdd899a8ae229348f53b89f70aeaf52e1ff82
Nerijus Urbonas <nerijus.urbonas@makrosolutions.com>
2023-05-13 21:24:14 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import { Configuration, OpenAIApi } from "openai";
+import.meta.env.VITE_ORGANIZATION;
+import.meta.env.VITE_API_KEY;
+
+const configuration = new Configuration({
+  organization: import.meta.env.VITE_ORGANIZATION,
+  apiKey: import.meta.env.VITE_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [chats, setChats] = useState([]);
+  const [isTyping, setIsTyping] = useState(false);
+
+  const chat = async (e, message) => {
+    e.preventDefault();
+
+    if (!message) return;
+    setIsTyping(true);
+    let msgs = chats;
+    msgs.push({ role: "user", content: message });
+    setChats(msgs);
+
+    setMessage("");
+    await openai
+      .createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content: "You are a Nerijus-GPT. You can help job tasks",
+          },
+          ...chats,
+        ],
+      })
+      .then((res) => {
+        msgs.push(res.data.choices[0].message);
+        setChats(msgs);
+        setIsTyping(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <main>
+      <h1>Chat AI from Nerijus</h1>
+      <section>
+        {chats && chats.length
+          ? chats.map((chat, index) => (
+              <p key={index} className={chat.role === "user" ? "user_msg" : ""}>
+                <span>
+                  <b>{chat.role.toUpperCase()}</b>
+                </span>
+                <span>:</span>
+                <span>{chat.content}</span>
+              </p>
+            ))
+          : ""}
+      </section>
+      <div className={isTyping ? "" : "hide"}>
+        <p>
+          <i>{isTyping ? "Typing" : ""}</i>
+        </p>
+      </div>
+      <form action="" onSubmit={(e) => chat(e, message)}>
+        <input
+          type="text"
+          name="message"
+          value={message}
+          placeholder="Type a message here and hit Enter..."
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </form>
+    </main>
+  );
+}
+export default App;
